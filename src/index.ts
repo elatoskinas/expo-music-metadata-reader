4479fe6487582b425de76c2fee777a87eee030fa
Evaldas Latoskinas <evaldasl61@gmail.com>
2023-09-17 13:40:48 +0200

    feat: setup boilerplate expo module

---
+++ src/index.ts
@@ -0,0 +1,24 @@
+import { NativeModulesProxy, EventEmitter, Subscription } from 'expo-modules-core';
+
+// Import the native module. On web, it will be resolved to ExpoMusicMetadataReader.web.ts
+// and on native platforms to ExpoMusicMetadataReader.ts
+import ExpoMusicMetadataReaderModule from './ExpoMusicMetadataReaderModule';
+import { ChangeEventPayload, ExpoMusicMetadataReaderViewProps } from './ExpoMusicMetadataReader.types';
+
+// TODO: implement functions / events
+
+// export function hello(): string {
+//   return ExpoMusicMetadataReaderModule.hello();
+// }
+
+// export async function setValueAsync(value: string) {
+//   return await ExpoMusicMetadataReaderModule.setValueAsync(value);
+// }
+
+// const emitter = new EventEmitter(ExpoMusicMetadataReaderModule ?? NativeModulesProxy.ExpoMusicMetadataReader);
+
+// export function addChangeListener(listener: (event: ChangeEventPayload) => void): Subscription {
+//   return emitter.addListener<ChangeEventPayload>('onChange', listener);
+// }
+
+// export { ExpoMusicMetadataReaderViewProps, ChangeEventPayload };
